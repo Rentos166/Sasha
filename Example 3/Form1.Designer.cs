158811ed9580fd4c1ba8d0d5049940ca57e68890
nikol <nikol@DESKTOP-OUT355N>
2022-06-25 06:34:03 +0300

    Добавьте файлы проекта.

---
+++ Example 3/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Example_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(482, 391);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(474, 365);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Вычисление матрицы D";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(429, 196);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(140, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 38);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Матрица D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 38);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Матрица С";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Матрица В";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Матрица А";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(474, 365);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Определитель";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(17, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(429, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.Location = new System.Drawing.Point(17, 147);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(429, 210);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(345, 25);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 38);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Выход";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(184, 25);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 38);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Вычисление";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 38);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Матрица";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 387);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Матрицы";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Example 3/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Example_3
+{
+    public partial class Form1 : Form
+    {
+        //String[] St;
+        //Класс List<string> - коллекция строк -   лист из строк имеет методы 
+        //накопления и преобразования неопределенного количества элементов 
+        List<string> list_st = new List<string>();
+        string[] St;
+        Double[,] MatrD, MatrA, MatrB, MatrBT, MatrC, Matr1, Matr2, Matr3, Matr4;
+        int i = 0, j = 0, n = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Матрица A
+            dataGridView1.Columns.Clear();
+            Sc(ref MatrA);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Матрица B
+            dataGridView1.Columns.Clear();
+            Sc(ref MatrB);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Матрица C
+            dataGridView1.Columns.Clear();
+            Sc(ref MatrC);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Матрица D=(A+B)*C
+            if ((dataGridView1.Columns.Count < 1) || MatrA == null || MatrB == null || MatrC == null) return;
+            dataGridView1.Columns.Clear();
+            //Добавление первой колонки 
+            DataGridViewTextBoxColumn titleColumn = new DataGridViewTextBoxColumn();
+            titleColumn.HeaderText = "Titlt" + 1.ToString();
+            titleColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns.Add(titleColumn);
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Добавление колонки
+                DataGridViewTextBoxColumn titleColumn1 = new DataGridViewTextBoxColumn();
+                titleColumn1.HeaderText = "Titlt" + (i + 2).ToString();
+                titleColumn1.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridView1.Columns.Add(titleColumn1);
+            }
+            MatrD = new Double[n, n];
+            Matr1 = new Double[n, n];
+            Matr2 = new Double[n, n];
+            MatrBT = new Double[n, n];
+            for (i = 0; i < n; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                        Matr1[i, j] = MatrA[i, k] * MatrB[k, j];
+                }
+            }
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    MatrBT[j, i] = MatrB[i, j];
+                }
+            }
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                        Matr2[i, j] = MatrC[i, k] - MatrBT[k,j];
+                }
+            }
+            
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    MatrD[i, j] = 0;
+                        MatrD[i, j] = MatrD[i, j] + Matr1[i, j] + Matr2[i, j];
+                    dataGridView1.Rows[i].Cells[j].Value = MatrD[i, j];
+                }
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Заполнение второй таблицы            
+            if (dataGridView1.Columns.Count < 1) return;
+            dataGridView2.Columns.Clear();
+            //Добавление первой колонки и первой строки
+            DataGridViewTextBoxColumn titleColumn = new DataGridViewTextBoxColumn();
+            titleColumn.HeaderText = "Titlt" + 1.ToString();
+            titleColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView2.Columns.Add(titleColumn);
+            dataGridView2.Rows.Add();
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Добавление колонки
+                DataGridViewTextBoxColumn titleColumn1 = new DataGridViewTextBoxColumn();
+                titleColumn1.HeaderText = "Titlt" + (i + 2).ToString();
+                titleColumn1.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridView2.Columns.Add(titleColumn1);
+                // добавление строки
+                dataGridView2.Rows.Add();
+            }
+            // Создание матрицы из второй таблицы
+            Matr3 = new Double[n, n];
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = dataGridView1.Rows[i].Cells[j].Value;
+                    Matr3[i, j] = Convert.ToDouble(dataGridView2.Rows[i].Cells[j].Value);
+                }
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Вычисление определителя
+            textBox1.Clear();
+            double S = Opred(Matr3, n);
+            textBox1.Text = "Определитель равен  " + Convert.ToString(S);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        //Процедура считывания строк из файла
+        private void Sc(ref Double[,] Matr)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream FS = openFileDialog1.OpenFile() as FileStream;
+                StreamReader SR = new StreamReader(FS);
+                dataGridView1.Rows.Clear();
+                //Добавление первой колонки 
+                DataGridViewTextBoxColumn titleColumn = new DataGridViewTextBoxColumn();
+                //Изменение заголовка колонки
+                titleColumn.HeaderText = "Titlt" + 1.ToString();
+                //Выравнивание ширины колонки в соответствии с заданным стилем - AllCells
+                titleColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridView1.Columns.Add(titleColumn);
+                list_st.Clear();
+                while (SR.Peek() > 0)
+                {
+                    // Считывание строки и добавление колонки (свойство AllowUserToAddRows - False)
+                    list_st.Add(SR.ReadLine());
+                    //dataGridView1
+                    dataGridView1.Rows.Add();
+                }
+                St = list_st.ToArray();
+                n = St.Length;
+                Matr = new Double[n, n];
+                for (i = 0; i < n - 1; i++)
+                {
+                    // Добавление колонки
+                    DataGridViewTextBoxColumn titleColumn1 = new DataGridViewTextBoxColumn();
+                    titleColumn1.HeaderText = "Titlt" + (i + 2).ToString();
+                    //Выравнивание ширины колонки в соответствии с заданным стилем - AllCells
+                    titleColumn1.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    dataGridView1.Columns.Add(titleColumn1);
+                }
+                for (i = 0; i < n; i++)
+                {
+                    St[i] = St[i].Replace(".", ","); ;
+                    //Разбиение строки St  на подстроки
+                    string[] aa = St[i].Split(" \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    for (j = 0; j < aa.Length; j++)
+                    {
+                        //Заполнение матрицы и таблицы
+                        Matr[i, j] = Convert.ToDouble(aa[j]);
+                        dataGridView1.Rows[i].Cells[j].Value = Matr[i, j];
+                    }
+                }
+                SR.Close();
+                FS.Close();
+            }
+        }
+        //Функция вычисления определителя
+        double Opred(double[,] M, int m)
+        {
+            if (M == null)
+                return 0;
+            Double[,] M2;
+            M2 = new double[m - 1, m - 1];
+            int i1 = 0, j1 = 0, k = 0, i = 0, j = 0;
+            double Opr = 0;
+            //Тривиальное решение
+            if (m == 1)
+            {
+                return Opr = M[0, 0];
+            }
+            for (k = 0; k < m; k++)
+            {
+                //Разложение по элементам нулевой строки
+                i1 = 0;
+                for (i = 0; i < m; i++)
+                {
+                    j1 = 0;
+                    if (i != 0)   //Вычеркивание нулевой строки
+                    {
+                        for (j = 0; j < m; j++)
+                        {
+                            if (j != k)    //Вычеркивание k-го столбца
+                            {
+                                M2[i1, j1] = M[i, j];
+                                j1++;
+                            }
+                        }
+                        i1++;
+                    }
+                }
+                // Рекурсивное обращение
+                Opr = Opr + Math.Pow(-1, k) * M[0, k] * Opred(M2, m - 1);
+            }
+            return Opr;
+        }
+    }
+}
